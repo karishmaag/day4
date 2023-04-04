4a7a41f566f8664cb3585e0d1931c13036348e68
karishmaag <karishmaagopinathan@gmail.com>
2023-04-05 00:16:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Head1 from './component/Head1';
+import Nav from './component/Nav';
+import { Cart } from './component/Cart';
+import Section1 from './component/Section1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head1/>
+      <Nav/>
+      <Section1/>
+      <Cart/>
     </div>
   );
 }
